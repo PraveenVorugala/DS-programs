78a4e6fd48abbf7dc274aebde0c41c89f6f9df64
PraveenVorugala <77579219+PraveenVorugala@users.noreply.github.com>
2021-01-20 22:03:52 +0530

    Update stack using linked list.C

--- stack using linked list.C
+++ stack using linked list.C
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include<stdlib.h>
 struct node{
-    int p;                                              //p is for data that has to be stored in list
-    struct node *n;                              //n is  pointer, pointing to next element
+    int p;                                //p is for data that has to be stored in list
+    struct node *next;                      //n is  pointer, pointing to next element
 };
-struct node *surf =NULL;             //surf is pointer for travesing,push,pop
+struct node *surf =NULL;               //surf is pointer for travesing,push,pop
 void display(struct node *o)
-{                                                    //display fun is for displaying elements
+{                                      //display is for displaying elements
     while(o!=NULL)
     {
     printf("%d,",o->p);
-    o=o->n;
+    o=o->next;
     }
 }
 void push(int x)                           //node u is used to add elements.
 {
    struct node* u;
    u=(struct node *)malloc(sizeof(struct node));
-    u->p=x;
-    u->n=surf;
-    surf=u;
+    u->p=x;                       // adding elements to node 
+    u->next=surf;                 // next pointer pointing to last node 
+    surf=u;                      // surf pointing to latest node  
 }
 void pop()                               // for pop we make the surf  to  point to next element
 {
-    surf=surf->n;
+    surf=surf->next;   // element is deleted from stack by using surf 
 }
 void main()
 {
-  clrscr();
   push(5);
   push(3);
   push(13);
@@ -36,5 +35,4 @@ void main()
   push(2);
   pop();
   display(surf);
-getch();
 }
